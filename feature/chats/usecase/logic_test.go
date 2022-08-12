036c50f233749b7787ed951b5088e685a8ce04a5
faqihassyfa <faqihassyfa@gmail.com>
2022-08-13 02:39:48 +0700

    tambah unit test

--- feature/chats/usecase/logic_test.go
+++ feature/chats/usecase/logic_test.go
@@ -1,1 +1,23 @@
 package usecase
+
+import (
+	"lesgoobackend/domain"
+	"lesgoobackend/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSendChat(t *testing.T) {
+	repo := mocks.ChatData{}
+	usecase := New(&repo)
+	successSend := domain.Chat{Group_ID: "m4nt4p", User_ID: 1, Message: "Hello"}
+
+	t.Run("success add to cart", func(t *testing.T) {
+		repo.On("Insert", successSend).Return(nil).Once()
+
+		err := usecase.SendChats(successSend)
+		assert.Nil(t, err)
+		repo.AssertExpectations(t)
+	})
+}
--- feature/group_users/usecase/logic_test.go
+++ feature/group_users/usecase/logic_test.go
@@ -1,1 +1,31 @@
 package usecase
+
+import (
+	"errors"
+	"lesgoobackend/domain"
+	"lesgoobackend/mocks"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddJoined(t *testing.T) {
+	repo := mocks.Group_UserData{}
+	usecase := New(&repo)
+	successAdd := domain.Group_User{Group_ID: "m4nt4p", User_ID: 1, Longitude: 1.0, Latitude: 1.0}
+	failedAdd := domain.Group_User{Group_ID: "", User_ID: 0, Longitude: 0.0, Latitude: 0.0}
+
+	t.Run("success join", func(t *testing.T) {
+		repo.On("Joined", successAdd).Return(nil).Once()
+		err := usecase.AddJoined(successAdd)
+		assert.Nil(t, err)
+		repo.AssertExpectations(t)
+	})
+
+	t.Run("failed join", func(t *testing.T) {
+		repo.On("Joined", failedAdd).Return(errors.New("failed")).Once()
+		err := usecase.AddJoined(failedAdd)
+		assert.NotNil(t, err)
+		repo.AssertExpectations(t)
+	})
+}
