8669726cd3a5458173768c1dea8274dd29d24ef3
Atha Dhaiffathin <athadhaiffathin@gmail.com>
2022-08-15 10:09:27 +0700

    fix swagger and add status to group chats

--- feature/groups/data/query.go
+++ feature/groups/data/query.go
@@ -20,7 +20,7 @@ func New(db *gorm.DB) domain.GroupData {
 func (gd *groupData) GetChatsAndUsersLocation(groupID string) (domain.GetChatsAndUsersLocationResponse, error) {
 	var result domain.GetChatsAndUsersLocationResponse
 	result.Group_ID = groupID
-	gd.db.Raw("SELECT id, name FROM groups WHERE id = ?", groupID).Scan(&result)
+	gd.db.Raw("SELECT id, name, status FROM groups WHERE id = ?", groupID).Scan(&result)
 	gd.db.Raw("SELECT c.id, c.message, c.user_id, u.profile_img, u.username, c.created_at FROM chats c INNER JOIN users u ON u.id = c.user_id").Scan(&result.Chats)
 	gd.db.Raw("SELECT g.id, g.latitude, g.longitude, g.user_id, g.user_id, u.username, u.profile_img FROM group_users g INNER JOIN users u ON u.id = g.user_id").Scan(&result.Group_Users)
 	return result, nil
